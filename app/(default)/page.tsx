2d7e307e85a70a563a4ee7fc9e7c3715607bff0a
zynrax <1uisvsn0c@gmail.com>
2024-10-08 14:55:11 -0600

    Initial commit

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,21 @@
 export const metadata = {
-  title: "Syna",
-  description: "idk",
+  title: "Syna.cc",
+  description: "Best Free Discord Bot",
+  openGraph: {
+    type: 'website',
+    url: 'https://www.syna.cc/',
+    title: 'Syna.cc',
+    description: 'Best Free Discord Bot',
+    images: [
+      {
+        url: 'https://hichnefu.sirv.com/Screenshot%202024-10-08%20144509.png',
+        width: 1920,
+        height: 1080,
+        alt: 'Syna.cc Discord Bot'
+      },
+    ],
+  },
+  // no x
 };
 
 import Hero from "@/components/hero-home";
--- app/(features)/premium/layout.tsx
+++ app/(features)/premium/layout.tsx
@@ -1,7 +1,23 @@
 export const metadata = {
-    title: "Syna",
-    description: "Premium",
-  };
+  title: "Syna.cc",
+  description: "Best Free Discord Bot",
+  openGraph: {
+    type: 'website',
+    url: 'https://www.syna.cc/',
+    title: 'Syna.cc',
+    description: 'Best Free Discord Bot',
+    images: [
+      {
+        url: 'https://hichnefu.sirv.com/Screenshot%202024-10-08%20144509.png',
+        width: 1920,
+        height: 1080,
+        alt: 'Syna.cc Discord Bot'
+      },
+    ],
+  },
+  // no x
+};
+
   import Footer from "@/components/ui/footer";
   
   export default function FeaturesLayout({
