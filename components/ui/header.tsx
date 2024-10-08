c52e9c204e8c2f12dedf8a41af108a2578a36cd7
zynrax <1uisvsn0c@gmail.com>
2024-10-08 16:30:40 -0600

    comit

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
             </li>
             <li>
               <Link
-                href="/features"
+                href="features"
                 className="btn-sm relative bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
               >
                 Features
@@ -27,7 +27,7 @@ export default function Header() {
             </li>
             <li>
               <Link
-                href="/premium"
+                href="premium"
                 className="btn-sm relative bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
               >
                 Premium
