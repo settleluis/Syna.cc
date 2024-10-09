0c5712a71effeeff86aa859e450f80333cea0ae3
zynrax <1uisvsn0c@gmail.com>
2024-10-08 19:35:30 -0600

    int

--- (auth)/features/page.tsx
+++ (auth)/features/page.tsx
@@ -54,7 +54,7 @@ export default function FeaturesPage() {
             />
             <span className="absolute top-0 left-0 h-full flex items-center pl-3">
               <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-indigo-500">
-                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
               </svg>
             </span>
           </div>
--- components/feature-list.tsx
+++ components/feature-list.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface FeatureListProps {
   searchQuery: string;
